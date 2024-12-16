e8fc34dce531b1d42e9378948941d91fcc75d796
zane <39070793+zaneenders@users.noreply.github.com>
2024-12-16 15:39:42 -0700

    Get paths for current dir

--- Package.swift
+++ Package.swift
@@ -3,16 +3,27 @@ import PackageDescription
 
 let package = Package(
     name: "scribe",
+    platforms: [
+        .macOS("15.0")
+    ],
     dependencies: [
         /*
         swift package --disable-sandbox preview-documentation --target Scribe
         */
         .package(
             url: "https://github.com/apple/swift-docc-plugin.git",
-            from: "1.3.0")
+            from: "1.3.0"),
+        .package(
+            url: "https://github.com/apple/swift-nio.git",
+            from: "2.77.0"
+        ),
     ],
     targets: [
-        .target(name: "Scribe"),
+        .target(
+            name: "Scribe",
+            dependencies: [
+                .product(name: "_NIOFileSystem", package: "swift-nio")
+            ]),
         .executableTarget(
             name: "MyScribe",
             dependencies: [
--- Sources/Scribe/Scribe.swift
+++ Sources/Scribe/Scribe.swift
@@ -1,3 +1,6 @@
+import Foundation
+import _NIOFileSystem
+
 /// Define your environment
 public protocol Scribe {
     init()
@@ -5,8 +8,32 @@ public protocol Scribe {
 }
 
 extension Scribe {
-    public static func main() {
+    public static func main() async {
         let scribe = self.init()
         print(scribe.config.hello)
+        var currenDir = FilePath(FileManager.default.currentDirectoryPath)
+        do {
+            for path in try await getDirectoryEntries(for: currenDir) {
+                print(path.path.string)
+            }
+        } catch {
+            print("Scribe ERROR: \(error.localizedDescription)")
+        }
     }
+}
+
+func getDirectoryEntries(for dir: FilePath) async throws -> [DirectoryEntry] {
+    var paths: [DirectoryEntry] = []
+    let dir = try await FileSystem.shared.openDirectory(atPath: dir)
+    let entries = dir.listContents()
+    var i = entries.makeAsyncIterator()
+    var next = try await i.next()
+    repeat {
+        if let n = next {
+            paths.append(n)
+        }
+        next = try await i.next()
+    } while next != nil
+    try await dir.close()
+    return paths
 }
